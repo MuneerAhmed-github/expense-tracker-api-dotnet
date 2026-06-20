6e8ad4df950674cf718659d6dddf2bcb45753c24
muneerahmed0704@gmail.com <muneerahmed0704@gmail.com>
2026-06-20 20:19:29 +0530

    Expense Tracker API_CRUD WITH EF CORE AND SQL SERVER

---
+++ ExpenseTrackerAPI/Controllers/ExpensesController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseTrackerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExpensesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public ExpensesController(AppDbContext context)
+        {
+                _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetExpenses()
+        {
+            var expenses=_context.Expenses.ToList();
+            return Ok(expenses);
+        }
+
+        [HttpPost]
+        public IActionResult AddExpense(Expense newExpense)
+        {
+            _context.Expenses.Add(newExpense);
+            _context.SaveChanges();
+            return Ok(newExpense);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateExpense(int id, Expense updatedExpense)
+        {
+            var expense = _context.Expenses.FirstOrDefault(x => x.Id == id);
+            if (expense == null)
+                return NotFound("Expense Not Found !");
+            expense.Title=updatedExpense.Title;
+            expense.Amount=updatedExpense.Amount;
+            expense.Category=updatedExpense.Category;
+            expense.Date=updatedExpense.Date;
+            _context.SaveChanges();
+            return Ok(expense);
+
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteExpense(int id)
+        {
+            var expense=_context.Expenses.FirstOrDefault(x=>x.Id == id);
+            if (expense == null)
+                return NotFound("Expenses Not Found !");
+            _context.Expenses.Remove(expense);
+            _context.SaveChanges();
+            return Ok("Expenses Deleted  SuccesFully");
+        }
+    }
+}
---
+++ ExpenseTrackerAPI/Expense.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExpenseTrackerAPI
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Amount { get; set; }
+        public string? Category { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
